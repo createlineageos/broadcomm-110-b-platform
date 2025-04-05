249ccc6d90198b9f1fdc18b1a28d828b8e82309e
createlineageos <bennuribaykondu3@gmail.com>
2025-04-05 13:19:34 +0200

    Update rom.py

--- bin/utils32/rom.py
+++ bin/utils32/rom.py
@@ -1,15 +1,16 @@
-firmware = "broadex 1.0"
-broadcomm_device = "BROADCOMM 110"
+firmware = "broadex 1.00.1"
+broadcomm_device = "READCOMM 110"
 core_level = "1 ROM"
 
 import time
+import os
 
 print("SINGLE CORE FIRMWARE!")
 
 
-time.sleep(10)
+time.sleep(5)
 
-#ASCII banner, you chan change it, because this project is open-source :)
+#ASCII banner, you chan change it, this project is open-source :)
 
 ascii = r"""____                      _           
  |  _ \                    | |          
@@ -18,7 +19,7 @@ ascii = r"""____                      _
  | |_) | | | (_) | (_| | (_| |  __/>  < 
  |____/|_|  \___/ \__,_|\__,_|\___/_/\_\
                                         
-                             V1.0           """
+                             V1.00.1           """
                                         
 print(ascii)
 
@@ -33,5 +34,34 @@ while True:
 	if cmd == "sysfetch":
 		print("Firmware/ROM:", firmware, "Broadcomm device:", broadcomm_device, "Core-level:", core_level)
 		
+	elif cmd == "restart --session":
+		print("rebooting... session...")
+		time.sleep(3)
+		print("SINGLE CORE FIRMWARE!")
+		time.sleep(5)
+		print(ascii)
+		
+	elif cmd == "help":
+		print("restart --session: Restarts your session, sysfetch: Informations, sysclose: Turning off READCOMM 110, setname: Set your session name, device: Your READCOMM version, setdevname: Set your device name, clear: Clear logs, timeinfo: Shows current date")
+		
+	elif cmd == "setname":
+		session = input("Session name: ")
+		print(session, "selected successfully")
+		
+	elif cmd == "device":
+		print(broadcomm_device)
+		
+	elif cmd == "setdevname":
+		devname = input("Select a device name: ")
+		print(devname, "selected successfully")
+		
+	elif cmd == "clear":
+		os.system("cls" if os.name == "nt" else "clear")
+	
+		
+	elif cmd == "timeinfo":
+		current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+		print("Current time:", current_time)
+		
 	else:
-		print("Command",cmd, "not found")
+		print("Command",cmd, "not found, type help to get list of commands")
